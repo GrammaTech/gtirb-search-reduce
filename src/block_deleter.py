6e2178e307987c9d08edf7b96ae58a283c813c4a
Jeremy Lacomis <jlacomis@grammatech.com>
2019-06-19 11:07:33 -0400

    Move Delta Debugging deletion to its own file

--- src/block_deleter.py
+++ src/block_deleter.py
@@ -8,6 +8,53 @@ import pprint
 from gtirb import *
 
 
+def block_addresses(ir):
+    blocks = list()
+    for module in ir._modules:
+        blocks += [b._address for b in module._blocks
+                   if hasattr(b, '_address')]
+    return blocks
+
+
+def get_function_map(ir):
+    """Returns a mapping from function (symbol) names to function UUIDs"""
+    # Symbol Name -> Function UUID
+    functions = dict()
+    for module in ir.modules():
+        # Function UUID -> set of entry block UUIDs
+        function_entries = module.auxData('functionEntries')
+        # Symbol Name -> Block UUID
+        block_symbols = {s.name(): s.referent().uuid()
+                          for s in module.symbols()
+                          if isinstance(s.referent(), Block)}
+        for symbol_name, block_uuid in block_symbols.items():
+            for function_uuid, entry_block_uuids in function_entries.items():
+                if block_uuid in entry_block_uuids:
+                    functions[symbol_name] = function_uuid
+                    break
+    return functions
+
+
+def get_function_block_addresses(function_name, functions, ir):
+    """Returns the set of block addresses corresponding to the function
+    with name function_name, given a mapping from function (symbol) names
+    to function UUIDs"""
+    log.debug(f"Getting blocks for function {function_name}")
+    function_uuid = functions[function_name]
+    # Block UUID -> Block Address
+    block_uuid_map = dict()
+    for module in ir.modules():
+        block_uuid_map.update({b.uuid(): b.address() for b in module.blocks()
+                               if hasattr(b, '_address')})
+
+    function_block_uuids = set()
+    for module in ir.modules():
+        # Function UUID -> set of block UUIDs in the function
+        function_blocks = module.auxData('functionBlocks')
+        function_block_uuids.update(function_blocks.get(function_uuid))
+    return {block_uuid_map[uuid] for uuid in function_block_uuids}
+
+
 def add_edge(graph, source, target, edge):
     source_entry = graph.get(source)
     if source_entry is not None:
@@ -156,27 +203,16 @@ def remove_blocks(ir, factory, block_addresses=list()):
             del function_blocks[key]
 
 
-def print_graph(graph):
-    for entry, value in graph.items():
-        def print_edges(direction):
-            for node, edges in value[direction].items():
-                edges = [f"({e._source_block._address:x},"
-                         f" {e._target_block._address:x})"
-                         for e in edges]
-                print(f"\t{direction.upper()}: {node._address:x} | "
-                      f"EDGES: {edges}")
-        try:
-            print(f"NODE: {entry._address:x}")
-            for direction in ['to', 'from']:
-                print_edges(direction)
-        except Exception as e:
-            pass
-        print()
-
-
-def block_addresses(ir):
-    blocks = list()
-    for module in ir._modules:
-        blocks += [b._address for b in module._blocks
-                   if hasattr(b, '_address')]
-    return blocks
+def remove_functions(ir, factory, function_names=list()):
+    """Takes a list of function names and deletes them"""
+    delete_blocks = set()
+    functions = get_function_map(ir)
+    for f in function_names:
+        delete_blocks.update(
+            get_function_block_addresses(
+                function_name=f,
+                functions=functions,
+                ir=ir
+            )
+        )
+    remove_blocks(ir, factory, delete_blocks)
---
+++ src/search/DDDeleter.py
@@ -0,0 +1,200 @@
+import logging as log
+import pickle
+import tempfile
+import shutil
+import subprocess
+import sys
+import os
+
+from gtirb import *
+
+import block_deleter
+from search.DD import DD, Result
+
+
+class DDDeleter(DD):
+    def __init__(self, infile, trampoline, workdir, save_files):
+        DD.__init__(self)
+        self.infile = infile
+        self.trampoline = trampoline
+        self.workdir = workdir
+        self.save_files = save_files
+        self._read_ir()
+        self.original_size = self._get_original_file_size()
+        if self.original_size is None:
+            sys.exit(f"Error: Could not build original file")
+        self.test_count = 0
+
+    def _read_ir(self):
+        if not os.path.exists(self.infile):
+            sys.exit(f"Error: Input file {self.infile} does not exist")
+        self._ir_loader = IRLoader()
+        self._ir = self._ir_loader.IRLoadFromProtobufFileName(self.infile)
+        self._factory = self._ir_loader._factory
+
+    def _get_original_file_size(self):
+        log.info("Getting original file size")
+        with tempfile.NamedTemporaryFile(suffix='ir') as ir_file, \
+             tempfile.NamedTemporaryFile(suffix='.S') as asm, \
+             tempfile.NamedTemporaryFile(suffix='exe') as exe:
+            ir_file.write(self._ir.toProtobuf().SerializeToString())
+            ir_file.flush()
+            # Dump assembly
+            pprinter_command = ['gtirb-pprinter',
+                                '-i', ir_file.name,
+                                '-o', asm.name]
+            try:
+                res = subprocess.run(pprinter_command,
+                                     stdout=subprocess.DEVNULL,
+                                     stderr=subprocess.DEVNULL)
+                if res.returncode != 0:
+                    log.error(f"gtirb-pprinter failed to assemble {asm}")
+                    return None
+            except Exception:
+                log.error("Exception while running gtirb-pprinter")
+                return None
+
+            # Compile
+            build_command = ['gcc', '-no-pie',
+                             asm.name, self.trampoline,
+                             '-o', exe.name]
+            try:
+                res = subprocess.run(build_command,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+                if res.returncode != 0:
+                    log.error(f"gcc failed to build with error:\n"
+                              f"{res.stderr.decode('utf-8').strip()}")
+                    return None
+            except Exception:
+                log.error("Exception while running gcc")
+                return None
+
+            # Get file size
+            return os.stat(exe.name).st_size
+
+    def _test(self, items):
+        items = set(items)
+        delete_items = [x for x in self.items if x not in items]
+        delete_items_list = ' '.join([str(b) for b in delete_items])
+        self.test_count += 1
+        log.info(f"Test #{self.test_count}")
+        log.debug(f"Processing: \n{delete_items_list}")
+
+        # Copy IR
+
+        # I (Jeremy) profiled pickle.loads(pickle.dumps()) and copy.deepcopy()
+        # and found that the pickle/unpickle method is about 5x faster. I think
+        # this is because deepcopy() has a lot of bookkeeping for corner cases.
+        log.info("Copying IR")
+        ir = pickle.loads(pickle.dumps(self._ir))
+
+        # Generate new IR
+        self._delete(ir, self._factory, delete_items)
+
+        # Output to a GTIRB file
+        with tempfile.TemporaryDirectory(prefix=str(self.test_count) + '-',
+                                         dir=self.workdir) as cur_dir, \
+             open(os.path.join(cur_dir, 'deleted.txt'), 'w+b') as item_list, \
+             open(os.path.join(cur_dir, 'out.ir'), 'w+b') as ir_file:
+
+            # Save if needed
+            def finish_test(test_result):
+                def copy_dir(dst):
+                    try:
+                        shutil.copytree(src=cur_dir, dst=dst)
+                    except OSError as e:
+                        log.error("Error copying "
+                                  f"{cur_dir} to {dst}:\n"
+                                  f"{e}")
+                result = {Result.FAIL: 'pass',
+                          Result.PASS: 'fail'}[test_result]
+                save_dir = os.path.join(self.workdir,
+                                        'results',
+                                        result,
+                                        str(self.test_count))
+                if self.save_files == 'all':
+                    copy_dir(save_dir)
+                elif self.save_files == 'passing' and result == 'fail':
+                    copy_dir(save_dir)
+                log.info(result.upper())
+                return test_result
+
+            asm = os.path.join(cur_dir, 'out.S')
+            exe = os.path.join(cur_dir, 'out.exe')
+            item_list.write(delete_items_list.encode())
+            ir_file.write(ir.toProtobuf().SerializeToString())
+            ir_file.flush()
+            # Dump assembly
+            log.info("Dumping assembly")
+            pprinter_command = ['gtirb-pprinter',
+                                '-i', ir_file.name,
+                                '-o', asm]
+            try:
+                res = subprocess.run(pprinter_command,
+                                     stdout=subprocess.DEVNULL,
+                                     stderr=subprocess.DEVNULL)
+                if res.returncode != 0:
+                    log.error(f"gtirb-pprinter failed to assemble {asm}")
+                    return finish_test(Result.PASS)
+            except Exception:
+                log.error("Exception while running gtirb-pprinter")
+                return finish_test(Result.PASS)
+
+            # Compile
+            log.info("Compiling")
+            build_command = ['gcc', '-no-pie',
+                             asm, self.trampoline,
+                             '-o', exe]
+            try:
+                res = subprocess.run(build_command,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+                if res.returncode != 0:
+                    log.info(f"gcc failed to build with error:\n"
+                             f"{res.stderr.decode('utf-8').strip()}")
+                    return finish_test(Result.PASS)
+            except Exception:
+                log.error("exception while running gcc")
+                return finish_test(Result.PASS)
+
+            # Run tests
+            log.info("Testing")
+            test_command = ['tests/test.py', exe]
+            res = subprocess.run(test_command)
+            if res.returncode != 0:
+                return finish_test(Result.PASS)
+            else:
+                log.debug("Deleted:\n"
+                          f"{delete_items_list}")
+                new_size = os.stat(exe).st_size
+                finish_test(Result.FAIL)
+                log.info(f"New file size: {new_size}, "
+                         f"{new_size / self.original_size * 100:.2f}% "
+                         "of original size")
+                return Result.FAIL
+
+    def _delete(self, items):
+        raise NotImplementedError
+
+
+class DDBlocks(DDDeleter):
+    def __init__(self, infile, trampoline, workdir, save_files):
+        super().__init__(infile, trampoline, workdir, save_files)
+        self.blocks = list(block_deleter.block_addresses(self._ir))
+        self.items = self.blocks
+
+    def _delete(self, ir, factory, blocks):
+        log.info("Deleting blocks")
+        block_deleter.remove_blocks(ir, factory, blocks)
+
+
+class DDFunctions(DDDeleter):
+    def __init__(self, infile, trampoline, workdir, save_files):
+        super().__init__(infile, trampoline, workdir, save_files)
+        self.functions = list(block_deleter.get_function_map(self._ir).keys())
+        self.items = self.functions
+
+    def _delete(self, ir, factory, functions):
+        log.info("Deleting functions")
+        block_deleter.remove_functions(ir, factory, functions)
